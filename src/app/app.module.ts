5bacbaa9d1e36bfa7b0f134e12efbd7873d935b7
SavvasAlexLau <107072078+SavvasAlexLau@users.noreply.github.com>
2022-07-06 10:59:19 -0400

    using component inside component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-
+import { isNgTemplate } from '@angular/compiler';
+import { MediaItemComponent } from './media-item.component';
 @NgModule({
   imports: [
     BrowserModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    MediaItemComponent //adding MediaItemComponent to the module to render
+                       //but we have to add the selector for it inside app.component.html, <mw-media-item>
   ],
   bootstrap: [
     AppComponent
