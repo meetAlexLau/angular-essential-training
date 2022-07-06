81af80b161f97d621e31954b0af58cf1cca4dd86
SavvasAlexLau <107072078+SavvasAlexLau@users.noreply.github.com>
2022-07-06 10:09:53 -0400

    app component template and url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'mw-app',
-  template: '<h1>My App</h1>'
+  selector: 'mw-app', //angular finds <mw-app> 
+  templateUrl: './app.template.html'
+    //angular inserts template where <mw-app> is, use template:
+    //you can also use refer template as a different file that contains .html text, use templateUrl:
 })
 export class AppComponent {}
