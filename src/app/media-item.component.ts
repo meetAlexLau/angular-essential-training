069a0a9bdda92f319ef63614dd2fd8ffe95d73b9
SavvasAlexLau <107072078+SavvasAlexLau@users.noreply.github.com>
2022-07-06 14:57:13 -0400

    event binding, (click)

--- src/app/media-item.component.ts
+++ src/app/media-item.component.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './media-item.component.html',
   styleUrls: ['./media-item.component.css']
 })
-export class MediaItemComponent {}
+export class MediaItemComponent {
+  onDelete() { //function for deleting 
+    console.log("delete clicked")
+  }
+
+  
+}
