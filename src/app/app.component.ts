17434650dd11d866a50aadbfc2c1ab5e513eb3e7
SavvasAlexLau <107072078+SavvasAlexLau@users.noreply.github.com>
2022-07-06 10:19:48 -0400

    styles and url, similar to template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'mw-app',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  // styles: [`
+  //   h1 { color: #ffffff; }
+  //   .description {
+  //     font-style: italic;
+  //     color: green
+  //   }
+  // `] //inline css, uses styles:
+  styleUrls: ['./app.component.css']
+  /*
+  using url file, uses styleUrls:
+  styleUrls takes in an array, so multiple css files can be used
+  */
 })
 export class AppComponent {}
