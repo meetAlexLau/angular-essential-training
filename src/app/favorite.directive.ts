fd3648c5be274602e5fb4fa978babe8f7bb7f627
SavvasAlexLau <107072078+SavvasAlexLau@users.noreply.github.com>
2022-07-08 14:29:26 -0400

    @HostListener for mouse enter/leave, @HostBind to bind .css value to variable. Update on events

--- src/app/favorite.directive.ts
+++ src/app/favorite.directive.ts
@@ -1,10 +1,20 @@
-import { Directive, HostBinding, Input } from '@angular/core';
+import { Directive, HostBinding, Input, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[mwFavorite]'
 })
 export class FavoriteDirective {
   @HostBinding('class.is-favorite') isFavorite = true;
+  @HostBinding('class.is-favorite-hovering') hovering = false;
+  //host binding binds the css is-favorite-hovering to the variable 'hovering'
+  @HostListener('mouseenter') onMouseEnter() { //ang checks for these events
+      this.hovering = true;
+      //so when 'hovering' is set to true, then is-favorite-hovering is applied to the element
+      //where mwFavorite is applied as a property
+  }
+  @HostListener('mouseleave') onMouseLeave() {
+      this.hovering = false;
+  }
   @Input() set mwFavorite(value) {
     this.isFavorite = value;
   }
